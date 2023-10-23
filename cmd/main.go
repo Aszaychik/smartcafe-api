551a8c08a2efd8d0ba8ba72fa1d500dc0bc8fe02
AsZaychik <aszaychik@gmail.com>
2023-10-23 13:21:02 +0700

    init echo server with mysql setup

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,31 @@
 package main
+
+import (
+	"aszaychik/smartcafe-api/domain"
+	"aszaychik/smartcafe-api/internal/infrastructure/config"
+	"aszaychik/smartcafe-api/internal/infrastructure/database"
+	"fmt"
+
+	"github.com/labstack/echo/v4"
+	"github.com/sirupsen/logrus"
+)
+
+func main() {
+	cfg, err := config.LoadConfig()
+	if err != nil {
+		logrus.Fatal(err.Error())
+	}
+	
+	fmt.Println(cfg)
+
+	db, err := database.NewMySQLConnection(&cfg.MySQL)
+	if err != nil {
+			logrus.Fatal("Error connecting to MySQL:", err.Error())
+	}
+
+	db.AutoMigrate(&domain.Admin{}, &domain.Menu{})
+
+	e := echo.New()
+
+	e.Logger.Fatal(e.Start(":8080"))
+}
