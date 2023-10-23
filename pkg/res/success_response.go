049dcca20507e38337232d3e07f0908f485ea892
AsZaychik <aszaychik@gmail.com>
2023-10-23 15:29:02 +0700

    success response helper

---
+++ pkg/res/success_response.go
@@ -0,0 +1,24 @@
+package res
+
+import (
+	"aszaychik/smartcafe-api/domain/web"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
+func successResponse(ctx echo.Context, code int, message string, data any) error {
+	return ctx.JSON(code, web.WebResponse{
+		Code:    code,
+		Message: message,
+		Data:    data,
+	})
+}
+
+func StatusCreated(ctx echo.Context, message string, data any) error {
+	return successResponse(ctx, http.StatusCreated, message, data)
+}
+
+func StatusOK(ctx echo.Context, message string, data any) error {
+	return successResponse(ctx, http.StatusOK, message, data)
+}
