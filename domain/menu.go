a851a13bf8c71d8c1dcaf5b29ddbc55c3fb834ba
AsZaychik <aszaychik@gmail.com>
2023-10-25 11:14:06 +0700

    menu domain update

--- domain/menu.go
+++ domain/menu.go
@@ -4,7 +4,7 @@ import "gorm.io/gorm"
 
 type Menu struct {
 	*gorm.Model
-	ItemName        string `gorm:"type:varchar(255)" json:"item_name"`
+	ItemName        string `gorm:"type:varchar(255);uniqueIndex" json:"item_name"`
 	ItemPrice       int    `gorm:"type:int" json:"item_price"`
 	ItemDescription string `gorm:"type:varchar(255)" json:"item_description"`
 	ItemImage       string `gorm:"type:varchar(255)" json:"item_image"`
