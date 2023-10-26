874f54fbbdf5dad39fe1c1ff122b14b95cdd4a2c
AsZaychik <aszaychik@gmail.com>
2023-10-26 17:25:33 +0700

    feat(domain): add order

--- domain/order.go
+++ domain/order.go
@@ -1,1 +1,20 @@
 package domain
+
+import "time"
+
+type OrderStatus string
+
+const (
+	Pending    OrderStatus = "pending"
+	Preparing  OrderStatus = "preparing"
+	Completed  OrderStatus = "completed"
+)
+
+type Order struct {
+	ID          uint `gorm:"type:int;primarykey" json:"id"`
+	CustomerID  uint `gorm:"type:int" json:"customer_id"`
+	TotalPrice  int  `gorm:"type:decimal(10,2)" json:"total_price"`
+	OrderStatus OrderStatus `gorm:"type:varchar(255)" json:"order_status"`
+	OrderDate   time.Time `gorm:"type:datetime" json:"order_date"`
+	Customer Customer `gorm:"foreignKey:CustomerID" json:"customer"`
+}
