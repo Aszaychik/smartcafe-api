5dbef8405cdc9c710805ae6082c69c5ae198e18a
AsZaychik <aszaychik@gmail.com>
2023-10-27 15:27:41 +0700

    feat(order): order create routes

---
+++ internal/app/order/order_routes.go
@@ -0,0 +1,25 @@
+package order
+
+import (
+	"aszaychik/smartcafe-api/internal/interfaces"
+
+	"github.com/labstack/echo/v4"
+)
+
+type OrderRoutesImpl struct {
+	Echo        *echo.Echo
+	OrderHandler interfaces.OrderHandler
+}
+
+func NewOrderRoutes(e *echo.Echo, orderHandler interfaces.OrderHandler) interfaces.OrderRoutes {
+	return &OrderRoutesImpl{
+		Echo:        e,
+		OrderHandler: orderHandler,
+	}
+}
+
+func (ar *OrderRoutesImpl) Order() {
+	ordersGroup := ar.Echo.Group("orders")
+
+	ordersGroup.POST("", ar.OrderHandler.CreateOrderHandler)
+}
