e24038d176553c163dabff93c7690a41d268e11f
AsZaychik <aszaychik@gmail.com>
2023-10-27 15:26:44 +0700

    feat(interface): add order interface

--- internal/interfaces/order_interface.go
+++ internal/interfaces/order_interface.go
@@ -1,1 +1,25 @@
 package interfaces
+
+import (
+	"aszaychik/smartcafe-api/domain"
+	"aszaychik/smartcafe-api/domain/web"
+
+	"github.com/labstack/echo/v4"
+)
+
+type OrderRepository interface {
+	Save(order *domain.Order) (*domain.Order, error)
+	FindById(orderId int) (*domain.Order, error)
+}
+
+type OrderService interface {
+	CreateOrder(ctx echo.Context, request web.OrderCreateRequest) (*domain.Order, error)
+}
+
+type OrderHandler interface {
+	CreateOrderHandler(ctx echo.Context) error
+}
+
+type OrderRoutes interface {
+	Order()
+}
