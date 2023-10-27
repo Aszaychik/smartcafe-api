3e2ba123c2ad56e49097c996f4fe0eb167a0c0d3
AsZaychik <aszaychik@gmail.com>
2023-10-27 20:13:21 +0700

    feat(domain): update order add foreignkey payment

--- domain/order.go
+++ domain/order.go
@@ -18,4 +18,5 @@ type Order struct {
 	OrderDate   time.Time `gorm:"type:datetime" json:"order_date"`
 	Customer Customer `gorm:"foreignKey:CustomerID" json:"customer"`
 	Items []OrderItem `gorm:"foreignKey:OrderID" json:"items"`
+	OrderPayment OrderPayment `gorm:"foreignKey:OrderID" json:"payment"`
 }
