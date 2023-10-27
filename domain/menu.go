95d689e8c6967b1c158ca3a9ad6137464a524ff8
AsZaychik <aszaychik@gmail.com>
2023-10-27 15:25:16 +0700

    refactor(domain): update price type to float

--- domain/menu.go
+++ domain/menu.go
@@ -3,7 +3,7 @@ package domain
 type Menu struct {
 	ID              uint     `gorm:"type:int;primarykey" json:"id"`
 	ItemName        string   `gorm:"type:varchar(255);uniqueIndex" json:"item_name"`
-	ItemPrice       int      `gorm:"type:int" json:"item_price"`
+	ItemPrice       float64  `gorm:"type:float" json:"item_price"`
 	ItemDescription string   `gorm:"type:varchar(255)" json:"item_description"`
 	ItemImage       string   `gorm:"type:varchar(255)" json:"item_image"`
 	CategoryID      uint     `gorm:"type:int" json:"category_id"`
--- domain/order.go
+++ domain/order.go
@@ -13,8 +13,9 @@ const (
 type Order struct {
 	ID          uint `gorm:"type:int;primarykey" json:"id"`
 	CustomerID  uint `gorm:"type:int" json:"customer_id"`
-	TotalPrice  int  `gorm:"type:decimal(10,2)" json:"total_price"`
+	TotalPrice  float64  `gorm:"type:float" json:"total_price"`
 	OrderStatus OrderStatus `gorm:"type:varchar(255)" json:"order_status"`
 	OrderDate   time.Time `gorm:"type:datetime" json:"order_date"`
 	Customer Customer `gorm:"foreignKey:CustomerID" json:"customer"`
+	Items []OrderItem `gorm:"foreignKey:OrderID" json:"items"`
 }
