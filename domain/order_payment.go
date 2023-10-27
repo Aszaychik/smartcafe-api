8b1c8a46123dae65347bb7ae211978645522ae26
AsZaychik <aszaychik@gmail.com>
2023-10-28 01:07:55 +0700

    feat(domain): add payment status for accept notif

--- domain/order_payment.go
+++ domain/order_payment.go
@@ -12,3 +12,11 @@ type OrderPayment struct {
 	PaymentDate time.Time `gorm:"type:datetime" json:"payment_date"`
 	PaymentUrl string `gorm:"type:varchar(255)" json:"payment_url"`
 }
+
+type PaymentStatus struct {
+	PaymentId string `json:"payment_id"`
+	PaymentStatus string `json:"payment_status"`
+	PaymentMethod string `json:"payment_method"`
+	PaymentDate time.Time `json:"payment_date"`
+	OrderID uint `json:"order_id"`
+}
